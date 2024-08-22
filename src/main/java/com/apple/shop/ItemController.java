0f031d2677dc786458a3efb3a5e89177a458af42
Rlack97 <amalilith97@gmail.com>
2024-08-22 20:56:59 +0900

    item details pages

--- src/main/java/com/apple/shop/ItemController.java
+++ src/main/java/com/apple/shop/ItemController.java
@@ -5,10 +5,12 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 // 상품관련 컨트롤러 (비슷한 api는 모아두는게 좋음)
 @Controller
@@ -33,4 +35,45 @@ public class ItemController {
         model.addAttribute("items",result);
         return "list.html";
     }
+
+    // 상품추가
+    // 1. 이름/ 가격용 페이지와 폼
+    // 2. 전송버튼 누르면 + 서버에서 검사 + db에 저장
+
+    @GetMapping("/write")
+    String write() {
+        return "write.html";
+    }
+
+
+    @PostMapping("/item")
+    String addPost(@ModelAttribute Item item) {
+
+//        @RequestParam Map<String,String> formData
+//        String title = formData.get("title");
+//        Integer price = Integer.parseInt(formData.get("price"));
+//
+//        Item item = new Item();
+//        item.setTitle(title);
+//        item.setPrice(price);
+//
+
+        itemRepository.save(item);
+
+        return "redirect:/list";
+    }
+
+    @GetMapping("/detail/{id}")
+    String detail(@PathVariable Integer id, Model model){
+        // 데이터가 없는 경우를 대비한 Optional
+        Optional<Item> item = itemRepository.findById(id);
+        if(item.isPresent()){
+            model.addAttribute("item",item.get());
+        } else{
+            return "redirect:/list";
+        }
+
+        return "detail.html";
+    }
 }
+
